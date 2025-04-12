ce06cf3a1031f8a9b89538ee1a7acb4a5d10eb75
Omar-Nashaat <omarnashaat18@gmail.com>
2025-04-12 16:21:10 +0200

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex space-x-4">
-        <Link to="/" className="text-white hover:text-gray-300">Home</Link>
-        <Link to="/about" className="text-white hover:text-gray-300">About</Link>
+        <Link to="/register" className="text-white hover:text-gray-300">Register</Link>
+        <Link to="/login" className="text-white hover:text-gray-300">Login</Link>
       </div>
     </nav>
   );
