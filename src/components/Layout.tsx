522bd1729c6ce66589cf02f8537c60a2d1d05e3f
Omar-Nashaat <omarnashaat18@gmail.com>
2025-04-12 17:28:21 +0200

    remove navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar';
 const MillerCompanyLayOut: React.FC = () => {
     return (
         <>
-            <Navbar />
+            {/* <Navbar /> */}
             <Outlet />
         </>
     );
